ba1ea9d34988525cc1cf73cf04d46b4cadc44dd0
Ubuntu <azureuser@MasterVM.kcbpvzzyfdrexowhem52y55zwc.bx.internal.cloudapp.net>
2022-02-12 01:15:17 +0000

    Add main

---
+++ terraform/azure_vm/main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.96.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  features{}
+}
+
+resource "azurerm_resource_group" "myTFResourceGroup" {
+      name = "myTFResourceGroup"
+      location = "eastus"
+}
+resource "azurerm_virtual_network" "MyTFVnet" {
+    name = "MyTFVnet"
+    location = azurerm_resource_group.myTFResourceGroup.location
+    address_space = ["10.0.0.0/16"]
+    resource_group_name = azurerm_resource_group.myTFResourceGroup.name
+
+}
+resource "azurerm_subnet" "MyTFSubnet" {
+    name = "MyTFSubnet"
+    resource_group_name = azurerm_resource_group.myTFResourceGroup.name
+    virtual_network_name = azurerm_virtual_network.MyTFVnet.name
+    address_prefixes = ["10.0.2.0/24"]
+  
+}
+resource "azurerm_public_ip" "MyTFPublicIP" {
+  name                = "MyTFPublicIP"
+  resource_group_name = azurerm_resource_group.myTFResourceGroup.name
+  location            = azurerm_resource_group.myTFResourceGroup.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
+  
+}
+
+resource "azurerm_network_interface" "MyTFNetInterface" {
+  name = "MyTFNetInterface"
+  location = azurerm_resource_group.myTFResourceGroup.location
+  resource_group_name = azurerm_resource_group.myTFResourceGroup.name
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.MyTFSubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.MyTFPublicIP.id
+  }
+}
+
+resource "tls_private_key" "MyTFssh" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+}
+
+resource "azurerm_linux_virtual_machine" "myTFLinuxVM" {
+  count = 2
+  name                = "myTFLinuxVM${count.index}"
+  resource_group_name = azurerm_resource_group.myTFResourceGroup.name
+  location            = azurerm_resource_group.myTFResourceGroup.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.MyTFNetInterface.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.MyTFssh.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version = "latest" 
+   }
+}
